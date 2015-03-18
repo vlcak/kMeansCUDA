bce7a9b51e380e2bad58499ef78a31a33ed6aff6
Jakub Vlcek <jakub.vlcek@gmail.com>
2015-03-18 07:47:19 +0100

    Added more file parameters (size, cluster, distribution, data)

--- Comparer/Comparer/MainWindow.xaml.cs
+++ Comparer/Comparer/MainWindow.xaml.cs
@@ -14,11 +14,12 @@ namespace Comparer
     public partial class MainWindow
     {
         //private String fileName1, fileName2;
-        private Point[] points1 = null, points2 = null;
-        private String path;
+        private Point[] _points1, _points2;
+        private readonly String _path;
+
         public MainWindow()
         {
-            path = "..\\..\\..\\..\\";
+            _path = "..\\..\\..\\..\\";
 
             ProjectData[] projectData = new[]
             {
@@ -35,45 +36,63 @@ namespace Comparer
                 ComboBoxLocation1.Items.Add(project);
                 ComboBoxLocation2.Items.Add(project);
             }
-            ComboBoxDimension.Items.Add("2");
-            ComboBoxDimension.Items.Add("3");
-            ComboBoxDimension.Items.Add("4");
-            ComboBoxDimension.Items.Add("8");
-            ComboBoxDimension.SelectedItem = "3";
+
+            for (int i = 2; i <= 256; i = i <= 32 ? i*2 : i + 32)
+            {
+                ComboBoxDimension.Items.Add(i.ToString());
+            }
+            for (int i = 2; i <= 32; i *= 2)
+            {
+                ComboBoxCluster.Items.Add(i.ToString());
+            }
+            for (int i = 8096; i <= 1048576; i *= 2)
+            {
+                ComboBoxSize.Items.Add(string.Format("{0}K", i/1000));
+            }
+            ComboBoxDataType.Items.Add("Double");
+            ComboBoxDataType.Items.Add("Float");
+            ComboBoxDistribution.Items.Add("Normal");
+            ComboBoxDistribution.Items.Add("Uniform");
+
+            ComboBoxCluster.SelectedIndex = 0;
+            ComboBoxDataType.SelectedIndex = 0;
+            ComboBoxDimension.SelectedIndex = 0;
+            ComboBoxDistribution.SelectedIndex = 0;
+            ComboBoxSize.SelectedIndex = 0;
         }
 
         private void buttonLoadFiles1_Click(object sender, RoutedEventArgs e)
         {
-            var ofd = new OpenFileDialog { DefaultExt = ".dat", Filter = "Data files (*.dat)|*.dat" };
+            var ofd = new OpenFileDialog {DefaultExt = ".dat", Filter = "Data files (*.dat)|*.dat"};
 
             bool? result = ofd.ShowDialog();
 
 
             if (result != null && result.Value)
             {
-                points1 = loadData(ofd.FileName);
+                _points1 = loadData(ofd.FileName);
                 labelFileName1.Content = ofd.FileName;
-                if (points2 != null)
+                if (_points2 != null)
                 {
-                    ComparePoints(points1, points2);
+                    ComparePoints(_points1, _points2);
                 }
             }
         }
 
         private void buttonLoadFiles2_Click(object sender, RoutedEventArgs e)
         {
-            var ofd = new OpenFileDialog { DefaultExt = ".dat", Filter = "Data files (*.dat)|*.dat" };
+            var ofd = new OpenFileDialog {DefaultExt = ".dat", Filter = "Data files (*.dat)|*.dat"};
 
             bool? result = ofd.ShowDialog();
 
 
             if (result != null && result.Value)
             {
-                points2 = loadData(ofd.FileName);
+                _points2 = loadData(ofd.FileName);
                 labelFileName2.Content = ofd.FileName;
-                if (points1 != null)
+                if (_points1 != null)
                 {
-                    ComparePoints(points1, points2);
+                    ComparePoints(_points1, _points2);
                 }
             }
         }
@@ -115,7 +134,8 @@ namespace Comparer
             int differencesCount = 0;
             if (a.Length != b.Length)
             {
-                listBoxDifferentFiles.Items.Add("Files cointains different number of points (" + a.Length + " : " + b.Length + ")");
+                listBoxDifferentFiles.Items.Add("Files cointains different number of points (" + a.Length + " : " +
+                                                b.Length + ")");
                 ++differencesCount;
             }
             else
@@ -124,16 +144,23 @@ namespace Comparer
                 {
                     if (a[i].Cluster != b[i].Cluster)
                     {
-                        listBoxDifferentFiles.Items.Add("Points " + i + " are assigned to different cluster (" + a[i].Cluster + " : " + b[i].Cluster + ") distances from cluster: " + a[i].DistanceFromCluster + " " + b[i].DistanceFromCluster);
+                        listBoxDifferentFiles.Items.Add("Points " + i + " are assigned to different cluster (" +
+                                                        a[i].Cluster + " : " + b[i].Cluster +
+                                                        ") distances from cluster: " + a[i].DistanceFromCluster + " " +
+                                                        b[i].DistanceFromCluster);
                         ++differencesCount;
                     }
                     else
                     {
                         for (int j = 0; j < a[i].Coords.Length; j++)
                         {
-                            if (Double.IsNaN(a[i].Coords[j]) || Double.IsNaN(b[i].Coords[j]) || (Math.Abs(a[i].Coords[j] - b[i].Coords[j]) > 0.0001))
+                            if (Double.IsNaN(a[i].Coords[j]) || Double.IsNaN(b[i].Coords[j]) ||
+                                (Math.Abs(a[i].Coords[j] - b[i].Coords[j]) > 0.0001))
                             {
-                                listBoxDifferentFiles.Items.Add("Points " + i + " have different coordinate (coordinate: " + j + ", values: " + a[i].Coords[j] + " : " + b[i].Coords[j] + ")");
+                                listBoxDifferentFiles.Items.Add("Points " + i +
+                                                                " have different coordinate (coordinate: " + j +
+                                                                ", values: " + a[i].Coords[j] + " : " + b[i].Coords[j] +
+                                                                ")");
                                 ++differencesCount;
                             }
                         }
@@ -145,9 +172,9 @@ namespace Comparer
 
         private void ButtonRefresh_OnClick(object sender, RoutedEventArgs e)
         {
-            points1 = loadData(labelFileName1.Content.ToString());
-            points2 = loadData(labelFileName2.Content.ToString());
-            ComparePoints(points1, points2);
+            _points1 = loadData(labelFileName1.Content.ToString());
+            _points2 = loadData(labelFileName2.Content.ToString());
+            ComparePoints(_points1, _points2);
         }
 
         private void ButtonSwitch_OnClick(object sender, RoutedEventArgs e)
@@ -156,10 +183,14 @@ namespace Comparer
             string file2 = labelFileName2.Content.ToString();
             if (!file1.Contains("clusters"))
             {
-                file1 = file1.Insert(file1.LastIndexOf("means"), "clusters");
-                file1 = file1.Remove(file1.LastIndexOf("means"), 5);
-                file2 = file2.Insert(file2.LastIndexOf("means"), "clusters");
-                file2 = file2.Remove(file2.LastIndexOf("means"), 5);
+                if (file1.Length > file1.LastIndexOf("means", StringComparison.Ordinal))
+                    file1 = file1.Insert(file1.LastIndexOf("means", StringComparison.Ordinal), "clusters");
+                if (file1.Length > file1.LastIndexOf("means", StringComparison.Ordinal))
+                    file1 = file1.Remove(file1.LastIndexOf("means", StringComparison.Ordinal), 5);
+                if (file2.Length > file2.LastIndexOf("means", StringComparison.Ordinal))
+                    file2 = file2.Insert(file2.LastIndexOf("means", StringComparison.Ordinal), "clusters");
+                if (file2.Length > file2.LastIndexOf("means", StringComparison.Ordinal))
+                    file2 = file2.Remove(file2.LastIndexOf("means", StringComparison.Ordinal), 5);
             }
             else
             {
@@ -167,22 +198,24 @@ namespace Comparer
                 file2 = file2.Replace("clusters", "means");
             }
 
-            points1 = loadData(file1);
-            points2 = loadData(file2);
+            _points1 = loadData(file1);
+            _points2 = loadData(file2);
             labelFileName1.Content = file1;
             labelFileName2.Content = file2;
-            ComparePoints(points1, points2);
+            ComparePoints(_points1, _points2);
         }
 
         private void ComboBoxLocation1_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             string type = labelFileName2.Content.ToString().Contains("clusters") ? "clusters" : "means";
-            string filePath1 = string.Format("{0}{1}{2}{3}D.dat", path, ((ProjectData) e.AddedItems[0]).Path, type, ComboBoxDimension.SelectedItem);
-            points1 = loadData(filePath1);
+            string filePath1 = string.Format("{0}{1}{2}{3}{4}{5}D{6}{7}C.dat", _path, ((ProjectData)ComboBoxLocation1.SelectedItem).Path, type,
+                     ComboBoxDataType.SelectedItem.ToString()[0], ComboBoxDistribution.SelectedItem.ToString()[0], ComboBoxDimension.SelectedItem, ComboBoxSize.SelectedItem, ComboBoxCluster.SelectedItem);
+            
+            _points1 = loadData(filePath1);
             labelFileName1.Content = filePath1;
-            if (points2 != null)
+            if (_points2 != null)
             {
-                ComparePoints(points1, points2);
+                ComparePoints(_points1, _points2);
             }
         }
 
@@ -190,37 +223,143 @@ namespace Comparer
         {
             string type = labelFileName1.Content.ToString().Contains("clusters") ? "clusters" : "means";
 
-            string filePath2 = string.Format("{0}{1}{2}{3}D.dat", path, ((ProjectData)e.AddedItems[0]).Path, type, ComboBoxDimension.SelectedItem);
-            points2 = loadData(filePath2);
+            string filePath2 = string.Format("{0}{1}{2}{3}{4}{5}D{6}{7}C.dat", _path, ((ProjectData)ComboBoxLocation1.SelectedItem).Path, type,
+                     ComboBoxDataType.SelectedItem.ToString()[0], ComboBoxDistribution.SelectedItem.ToString()[0], ComboBoxDimension.SelectedItem, ComboBoxSize.SelectedItem, ComboBoxCluster.SelectedItem);
+            _points2 = loadData(filePath2);
             labelFileName2.Content = filePath2;
-            if (points1 != null)
+            if (_points1 != null)
             {
-                ComparePoints(points1, points2);
+                ComparePoints(_points1, _points2);
             }
         }
 
         private void ComboBoxDimension_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (ComboBoxLocation1.SelectedItem != null)
+            if (ComboBoxLocation1.SelectedItem != null && CheckSelectionBox())
             {
-                string filePath1 = string.Format("{0}{1}clusters{2}D.dat", path,
-                    ((ProjectData) ComboBoxLocation1.SelectedItem).Path, e.AddedItems[0]);
-                points1 = loadData(filePath1);
+                string filePath1 = string.Format("{0}{1}clusters{2}{3}{4}D{5}{6}C.dat", _path, ((ProjectData)ComboBoxLocation1.SelectedItem).Path,
+                    ComboBoxDataType.SelectedItem.ToString()[0], ComboBoxDistribution.SelectedItem.ToString()[0], ComboBoxDimension.SelectedItem, ComboBoxDimension.SelectedItem, ComboBoxCluster.SelectedItem);
+                _points1 = loadData(filePath1);
                 labelFileName1.Content = filePath1;
             }
 
-            if (ComboBoxLocation2.SelectedItem != null)
+            if (ComboBoxLocation2.SelectedItem != null && CheckSelectionBox())
             {
-                string filePath2 = string.Format("{0}{1}clusters{2}D.dat", path,
-                    ((ProjectData) ComboBoxLocation2.SelectedItem).Path, e.AddedItems[0]);
-                points2 = loadData(filePath2);
+                string filePath2 = string.Format("{0}{1}clusters{2}{3}{4}D{5}{6}C.dat", _path, ((ProjectData)ComboBoxLocation2.SelectedItem).Path,
+                    ComboBoxDataType.SelectedItem.ToString()[0], ComboBoxDistribution.SelectedItem.ToString()[0], ComboBoxDimension.SelectedItem, ComboBoxDimension.SelectedItem, ComboBoxCluster.SelectedItem);
+                _points2 = loadData(filePath2);
                 labelFileName2.Content = filePath2;
             }
 
-            if (points1 != null && points2 != null)
+            if (_points1 != null && _points2 != null)
             {
-                ComparePoints(points1, points2);
+                ComparePoints(_points1, _points2);
             }
+        }
+
+        private void ComboBoxSize_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ComboBoxLocation1.SelectedItem != null && CheckSelectionBox())
+            {
+                string filePath1 = string.Format("{0}{1}clusters{2}{3}{4}D{5}{6}C.dat", _path, ((ProjectData)ComboBoxLocation1.SelectedItem).Path,
+                    ComboBoxDataType.SelectedItem.ToString()[0], ComboBoxDistribution.SelectedItem.ToString()[0], ComboBoxDimension.SelectedItem, e.AddedItems[0], ComboBoxCluster.SelectedItem);
+                _points1 = loadData(filePath1);
+                labelFileName1.Content = filePath1;
+            }
+
+            if (ComboBoxLocation2.SelectedItem != null && CheckSelectionBox())
+            {
+                string filePath2 = string.Format("{0}{1}clusters{2}{3}{4}D{5}{6}C.dat", _path, ((ProjectData) ComboBoxLocation2.SelectedItem).Path,
+                    ComboBoxDataType.SelectedItem.ToString()[0], ComboBoxDistribution.SelectedItem.ToString()[0], ComboBoxDimension.SelectedItem, e.AddedItems[0], ComboBoxCluster.SelectedItem);
+                _points2 = loadData(filePath2);
+                labelFileName2.Content = filePath2;
+            }
+
+            if (_points1 != null && _points2 != null)
+            {
+                ComparePoints(_points1, _points2);
+            }
+        }
+
+        private void ComboBoxCluster_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ComboBoxLocation1.SelectedItem != null && CheckSelectionBox())
+            {
+                string filePath1 = string.Format("{0}{1}clusters{2}{3}{4}D{5}{6}C.dat", _path, ((ProjectData)ComboBoxLocation1.SelectedItem).Path,
+                    ComboBoxDataType.SelectedItem.ToString()[0], ComboBoxDistribution.SelectedItem.ToString()[0], ComboBoxDimension.SelectedItem, ComboBoxDimension.SelectedItem, e.AddedItems[0]);
+                _points1 = loadData(filePath1);
+                labelFileName1.Content = filePath1;
+            }
+
+            if (ComboBoxLocation2.SelectedItem != null && CheckSelectionBox())
+            {
+                string filePath2 = string.Format("{0}{1}clusters{2}{3}{4}D{5}{6}C.dat", _path, ((ProjectData)ComboBoxLocation2.SelectedItem).Path,
+                    ComboBoxDataType.SelectedItem.ToString()[0], ComboBoxDistribution.SelectedItem.ToString()[0], ComboBoxDimension.SelectedItem, ComboBoxDimension.SelectedItem, e.AddedItems[0]);
+                _points2 = loadData(filePath2);
+                labelFileName2.Content = filePath2;
+            }
+
+            if (_points1 != null && _points2 != null)
+            {
+                ComparePoints(_points1, _points2);
+            }
+        }
+
+        private void ComboBoxDistribution_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ComboBoxLocation1.SelectedItem != null && CheckSelectionBox())
+            {
+                string filePath1 = string.Format("{0}{1}clusters{2}{3}{4}D{5}{6}C.dat", _path, ((ProjectData)ComboBoxLocation1.SelectedItem).Path,
+                    ComboBoxDataType.SelectedItem.ToString()[0], e.AddedItems[0].ToString()[0], ComboBoxDimension.SelectedItem, ComboBoxDimension.SelectedItem, ComboBoxCluster.SelectedItem);
+                _points1 = loadData(filePath1);
+                labelFileName1.Content = filePath1;
+            }
+
+            if (ComboBoxLocation2.SelectedItem != null && CheckSelectionBox())
+            {
+                string filePath2 = string.Format("{0}{1}clusters{2}{3}{4}D{5}{6}C.dat", _path, ((ProjectData)ComboBoxLocation2.SelectedItem).Path,
+                    ComboBoxDataType.SelectedItem.ToString()[0], e.AddedItems[0].ToString()[0], ComboBoxDimension.SelectedItem, ComboBoxDimension.SelectedItem, ComboBoxCluster.SelectedItem);
+                _points2 = loadData(filePath2);
+                labelFileName2.Content = filePath2;
+            }
+
+            if (_points1 != null && _points2 != null)
+            {
+                ComparePoints(_points1, _points2);
+            }
+        }
+
+        private void ComboBoxDataType_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ComboBoxLocation1.SelectedItem != null && CheckSelectionBox())
+            {
+                string filePath1 = string.Format("{0}{1}clusters{2}{3}{4}D{5}{6}C.dat", _path, ((ProjectData)ComboBoxLocation1.SelectedItem).Path,
+                    e.AddedItems[0].ToString()[0], ComboBoxDistribution.SelectedItem.ToString()[0], ComboBoxDimension.SelectedItem, ComboBoxDimension.SelectedItem, ComboBoxCluster.SelectedItem);
+                _points1 = loadData(filePath1);
+                labelFileName1.Content = filePath1;
+            }
+
+            if (ComboBoxLocation2.SelectedItem != null && CheckSelectionBox())
+            {
+                string filePath2 = string.Format("{0}{1}clusters{2}{3}{4}D{5}{6}C.dat", _path, ((ProjectData)ComboBoxLocation2.SelectedItem).Path,
+                    e.AddedItems[0].ToString()[0], ComboBoxDistribution.SelectedItem.ToString()[0], ComboBoxDimension.SelectedItem, ComboBoxDimension.SelectedItem, ComboBoxCluster.SelectedItem);
+                _points2 = loadData(filePath2);
+                labelFileName2.Content = filePath2;
+            }
+
+            if (_points1 != null && _points2 != null)
+            {
+                ComparePoints(_points1, _points2);
+            }
+        }
+
+        private bool CheckSelectionBox()
+        {
+            return (ComboBoxCluster.SelectedIndex != -1)
+                && (ComboBoxDataType.SelectedIndex != -1)
+                && (ComboBoxDimension.SelectedIndex != -1)
+                && (ComboBoxDistribution.SelectedIndex != -1)
+                && (ComboBoxSize.SelectedIndex != -1);
         }
     }
 
