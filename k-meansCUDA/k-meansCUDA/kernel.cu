08af5f6391476c3edef397aa38cec61c46a62238
Jakub Vlcek <jakub.vlcek@gmail.com>
2015-07-05 11:47:41 +0200

    Added shuffle function version

--- k-meansCUDA/k-meansCUDA/kernel.cu
+++ k-meansCUDA/k-meansCUDA/kernel.cu
@@ -214,7 +214,7 @@ __global__ void findNearestClusterManyDimUnrolledKernel(const uint32_t meansSize
 __global__ void findNearestClusterManyDimShuffleKernel(const uint32_t meansSize, const value_t *means, value_t *measnSums, const uint32_t dataSize, const value_t* data, uint32_t* counts, uint32_t* assignedClusters, const uint32_t dimension)
 {
     //int id = threadIdx.x;
-    /*value_t minDistance = LLONG_MAX, difference = 0, distance = 0;
+    value_t minDistance = LLONG_MAX, difference = 0, distance = 0;
     int clusterID = -1;
     extern __shared__ value_t distances[];
 
@@ -237,18 +237,15 @@ __global__ void findNearestClusterManyDimShuffleKernel(const uint32_t meansSize,
         //if (blockSize >= 256) { if (threadIdx.x < 128) { distances[threadIdx.x] += distances[threadIdx.x + 128]; } __syncthreads(); }
         //if (blockSize >= 128) { if (threadIdx.x <  64) { distances[threadIdx.x] += distances[threadIdx.x +  64]; } __syncthreads(); }
 
-        if (threadIdx.x < blockDim.x / 2)
-        {
-            //if (blockSize >= 64) distances[threadIdx.x] += distances[threadIdx.x + 32];
-            if (blockSize >= 32) distances[threadIdx.x] += distances[threadIdx.x + 16];
-            if (blockSize >= 16) distances[threadIdx.x] += distances[threadIdx.x + 8];
-            if (blockSize >= 8) distances[threadIdx.x] += distances[threadIdx.x + 4];
-            if (blockSize >= 4) distances[threadIdx.x] += distances[threadIdx.x + 2];
-            if (blockSize >= 2) distances[threadIdx.x] += distances[threadIdx.x + 1];
-        }
+		//for (int offset = warpSize / 2; offset > 0; offset /= 2)
+		if (warpSize > 32) distance += __shfl_down(distance, 32);
+		distance += __shfl_down(distance, 16);
+		distance += __shfl_down(distance, 8);
+		distance += __shfl_down(distance, 4);
+		distance += __shfl_down(distance, 2);
+		distance += __shfl_down(distance, 1);
 
-
-        if ((minDistance > distances[0]))
+        if ((minDistance > distance))
         {
             minDistance = distances[0];
             clusterID = i;
@@ -261,7 +258,7 @@ __global__ void findNearestClusterManyDimShuffleKernel(const uint32_t meansSize,
         assignedClusters[blockIdx.x] = clusterID;
     }
 
-    atomicAdd(&measnSums[clusterID * blockDim.x + threadIdx.x], data[blockIdx.x * blockDim.x + threadIdx.x]);*/
+    atomicAdd(&measnSums[clusterID * blockDim.x + threadIdx.x], data[blockIdx.x * blockDim.x + threadIdx.x]);
 }
 
 __global__ void findNearestClusterFewMeansKernel(const uint32_t meansSize, const value_t *means, value_t *measnSums, const uint32_t dataSize, const value_t* data, uint32_t* counts, uint32_t* assignedClusters, const uint32_t dimension)
